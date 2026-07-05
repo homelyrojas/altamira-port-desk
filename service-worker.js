da3453931fafe2e9816e0590189712fe72c4c2f1
homelyrojas <homelyrojas@gmail.com>
2026-07-05 13:13:46 -0600

    Hotfix: refresh service worker cache for study guide

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "altamira-port-desk-v143";
+const CACHE_NAME = "altamira-port-desk-v144-study-guide-hotfix";
 const ASSETS = [
   "./",
   "./index.html",
@@ -42,12 +42,14 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+      .then(() => self.clients.claim())
   );
 });
 
